e00ae1ccb027868130ff76894d9bc99096a8b861
angseung <angseung@sju.ac.kr>
2021-05-21 17:09:37 +0900

    TODO : Gini Index value inversion problem...

--- Data_Generation/get_gini_index.m
+++ Data_Generation/get_gini_index.m
@@ -1,19 +1,23 @@
 function [G] = get_gini_index(p)
-
+    
     N = length(p);
     temp_sum = 0;
-    abs_p = abs(p);
+    abs_p = abs(p .* (p').');
     l1_p = sum(abs_p);
     [~, p_ind] = sort(abs_p);
-    p_asd = abs(p(p_ind));
+    p_asd = abs_p(p_ind);
 
 
     for i = 1 : N
         curr_p = p_asd(i);
         temp_sum = temp_sum + (curr_p / l1_p) * ((N - i + 0.5) / N);
-    end 
+    end
 
     G = 1 - 2 * temp_sum;
+    
+    % Gini index value MUST be within [0, 1]...
+    isValid = (G >= 0) && (G <= 1);
+    assert(isValid)
     
 end
 
